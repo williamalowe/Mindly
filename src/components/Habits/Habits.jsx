1b52b081c7c7f4bcb7358a014944a9f526bf07ec
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-22 22:30:22 +1000

    feat: add header-component

--- src/components/Habits/Habits.jsx
+++ src/components/Habits/Habits.jsx
@@ -1,9 +1,12 @@
+import Header from '../Header/Header';
 import styles from './Habits.module.css';
 
 const Habits = () => {
   return (
     <div className={styles.habits}>
-      
+      <Header 
+        header='habits'
+      />
     </div>
   )
 }
